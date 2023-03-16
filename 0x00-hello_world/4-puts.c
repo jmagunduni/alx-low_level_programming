511c3fc4b036d83ea4d511f5b53dc38a05e15b4e
Jmagunduni <jmagunduni@gmail.com>
2023-03-16 14:42:27 -0700

    created a program which displays a message on the screen

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ * main - write a C program that prints using puts statement
+ * returns: 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}}
