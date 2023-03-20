d90f8e2c82d8741336caaacad8ccfe34d8a64c9b
Jmagunduni <jmagunduni@gmail.com>
2023-03-20 04:24:46 -0700

    hot fix change

---
+++ 0x00-hello_world/new_proj.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - sample program to do xyz
+ * Return : Always 0
+ */
+
+int main(void)
+{
+    char listAlphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+    int counter;
+
+    for (counter = 0 ; counter < 26 ; counter++)
+    {
+	printf("alphabet %d", listAlphabet[counter]);
+	return (0);
+    }
+    
+}
