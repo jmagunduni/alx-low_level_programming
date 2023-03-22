fa86501d9e123ebc020329d8393720d284c0910f
Jmagunduni <jmagunduni@gmail.com>
2023-03-22 16:24:26 -0700

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,14 @@
+/**
+* _isalpha - checks for alphabetic character.
+* @c: alphabet character
+* Return: Returns 1 if c is a letter, lowercase or uppercase
+* Returns 0 otherwise
+*/
+
 #include "main.h"
 
-/**
- * _isalpha - checks for alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, 0 otherwise
- */
-int _isalpha(int c)
-{
-    return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-}
+	int _isalpha(int c)
+	{
+		return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+	}
+
