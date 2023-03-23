698e5f1c929a0629bf59a793e019dcd6feaf782f
Jmagunduni <jmagunduni@gmail.com>
2023-03-22 20:46:47 -0700

    fibonacci seq

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,34 +1,35 @@
+/**
+* main- prints the fibonacci sequence starting
+* with 1 and 2,separated by a comma followed
+* by a space.
+* Description: Write a program that prints the
+* first 50 Fibonacci numbers, starting
+* with 1 and 2, followed by a new line.
+* Return: 0 ALWAYS
+*/
+
 #include <stdio.h>
 
-
-	/**
-	 * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
-	 *        separated by a comma followed by a space.
-	 *
-	 * Return: Always 0.
-	 */
 	int main(void)
 	{
-		int count;
-		unsigned long fib1 = 0, fib2 = 1, sum;
+		int counter;
+		unsigned long fibonacci1 = 0;
+		unsigned long fibonacci2 = 1;
+		unsigned long totalSum;
 
-
-		for (count = 0; count < 50; count++)
+		for (counter = 0; counter < 50; counter++)
 		{
-			sum = fib1 + fib2;
-			printf("%lu", sum);
+			totalSum = fibonacci1 + fibonacci2;
+			printf("%lu", totalSum);
 
+			fibonacci1 = fibonacci2;
+			fibonacci2 = totalSum;
 
-			fib1 = fib2;
-			fib2 = sum;
-
-
-			if (count == 49)
+			if (counter == 49)
 				printf("\n");
 			else
 				printf(", ");
 		}
-
 
 		return (0);
 	}
