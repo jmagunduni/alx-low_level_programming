a63ec5a54e79e764669245bad5f708d535f379ed
Jmagunduni <jmagunduni@gmail.com>
2023-03-28 12:19:20 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+#include "main.h"
+
+int _strlen(char *s)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+
+	return (longi);
+}
