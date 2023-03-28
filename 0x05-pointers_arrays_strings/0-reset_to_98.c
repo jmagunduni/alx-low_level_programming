77e844819aabda295f8e106b055a59c6e2563c49
Jmagunduni <jmagunduni@gmail.com>
2023-03-28 11:43:15 -0700

    added project

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 /**
- * reset_to_98 - a function that takes a pointer to an int as parameter
- * and updates the value it points to to 98
+ * reset_to_98 - int as parameter and updates
+ * the value it points to 98
  * @n: input
  * Return: n
  */
-#include "main.h"
 
-void reset_to_98(int *n)
-{
-	*n = 98;
-}
+#include "main.h"
+ void reset_to_98(int *n)
+ {
+	 *n = 98;
+ }
---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+/**
+ * swap_int - swaps the values of two integers
+ * @a: integer to swap
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int refValue;
+
+	refValue = *a;
+	*a = *b;
+	*b = refValue;
+}
