9517389448b11721d73f25a4aec50c1093dc7691
Jmagunduni <jmagunduni@gmail.com>
2023-04-15 18:50:27 -0700

    100-change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
 		while (enteredNumber >= arrayCoins[counter])
 		{
 			result++;
-			enteredNumber = enteredNumber - coins[counter];
+			enteredNumber = enteredNumber - arrayCoins[counter];
 		}
 	}
 
