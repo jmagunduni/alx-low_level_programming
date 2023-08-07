017bc9997af1d35f4fc6d95d89c968c453fa671e
jmagunduni <jmagunduni@gmail.com>
2023-08-07 13:48:10 -0700

    malloc challange project

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - main entry
+ * @ac: int input
+ * @av: double pointer array
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, k = 0, l = 0;
+	char *string;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			l++;
+	}
+	l += ac;
+
+	string = malloc(sizeof(char) * l + 1);
+	if (string == NULL)
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+	for (j = 0; av[i][j]; j++)
+	{
+		string[k] = av[i][j];
+		k++;
+	}
+	if (string[k] == '\0')
+	{
+		string[k++] = '\j';
+	}
+	}
+	return (string);
+}
