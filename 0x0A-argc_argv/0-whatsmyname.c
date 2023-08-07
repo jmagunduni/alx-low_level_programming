c4964ff0fee2e4ae34d5e0f582f7ddaed45611c8
jmagunduni <jmagunduni@gmail.com>
2023-08-07 14:05:53 -0700

    0x0A. C - argc, argv, project

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - this function prints the name
  * of the program
@@ -13,6 +12,5 @@
 int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", *argv);
-
 	return (0);
 }
