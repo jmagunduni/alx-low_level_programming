7c9b4d679adf01c32a5ff06d7666d7185b2f1a17
jmagunduni <jmagunduni@gmail.com>
2023-08-10 14:18:34 -0700

    Malloc,free more memory allocation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -27,9 +27,9 @@ while (s2 && s2[len2])
 len2++;
 
 if (n < len2)
-s = malloc(sizeof(char) * (len1 + n + 1));
+pntr = malloc(sizeof(char) * (len1 + n + 1));
 else
-s = malloc(sizeof(char) * (len1 + len2 + 1));
+pntr = malloc(sizeof(char) * (len1 + len2 + 1));
 
 if (!s)
 return (NULL);
