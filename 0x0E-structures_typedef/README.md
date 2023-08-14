c848e18e2926a9af116bbba38baa7123fa1ccde5
jmagunduni <jmagunduni@gmail.com>
2023-08-14 06:25:29 -0700

    structures project

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+Structures project ! by Joshua Magunduni
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - this is the structure of dog info
+* @name: the dogs first name
+* @age: the dogs last name
+* @owner: The name of the dogs owner
+*
+* Description: this structure contains all props
+*of a dog
+*/
+
+struct dog
+{
+char name;
+double age;
+char owner;
+}
+
+#endif
