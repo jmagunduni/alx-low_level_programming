0cbc600dfd66fbeab135819a45013ff9323299a2
jmagunduni <jmagunduni@gmail.com>
2023-08-14 04:53:59 -0700

    preprocessor and macros

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - This function makes use of standard c macros 
+ *to print the name of the fine 
+ *Return: Always 0(Success)
+ */
+int main(void )
+{
+printf("%s\n",__FILE__);
+return(0);
+}
