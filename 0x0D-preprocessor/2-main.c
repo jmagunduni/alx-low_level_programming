f6c4acb974dcd0884ce4b2b844819d6cdba7e262
jmagunduni <jmagunduni@gmail.com>
2023-08-14 05:43:38 -0700

    preprocessor and macro project

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- *main - This function makes use of standard c macros 
- *to print the name of the fine 
- *Return: Always 0(Success)
- */
-int main(void )
+*main - This function makes use of standard c macros
+*to print the name of the fine
+*Return: Always 0(Success)
+*/
+int main(void)
 {
-printf("%s\n",__FILE__);
-return(0);
+printf("%s\n", __FILE__);
+return (0);
 }
