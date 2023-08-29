fc07a6d6391783ba548eb7af1bd510bd67cb68a0
jmagunduni <jmagunduni@gmail.com>
2023-08-29 16:17:07 -0700

    0x13. C - More singly linked lists

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * reverse_listint - function reverses a linked list
+ * @head: pointer to the first node in the list
+ *
+ * Return: pointer to the first node in the new list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prevs_node= NULL;
+	listint_t *next_node = NULL;
+
+	while (*head)
+	{
+		next_node = (*head)->next;
+		(*head)->next = prevs_node;
+		prevs_node = *head;
+		*head = next_node;
+	}
+
+	*head = prevs_node;
+
+	return (*head);
+}
